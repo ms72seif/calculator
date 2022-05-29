9b1ef5dbd2f9a703887e5787a24c2c5170e81a28
Mohammad Sadegh Seif <106380528+ms72seif@users.noreply.github.com>
2022-05-30 02:12:54 +0430

    calculator
    calculator with javascript html

---
+++ js/master.js
@@ -0,0 +1,23 @@
+let sum = "";
+    let _res = document.getElementById('dsp');
+    let _yNm = document.getElementById('dsp1');
+    function _calc(e){
+        sum+=e;
+        _yNm.value=sum;
+    }
+    function _backspace(){
+    sum =sum.substr(0,sum.length - 1);
+    _yNm.value=sum;
+    }
+    function _reset(){
+        _yNm.value="";
+        _res.value="";
+        sum=""
+    }
+    function _result(){
+        if(_yNm.value!=""){
+            _res.value = eval(sum);
+        }else{
+            alert('please enter number')
+        }
+    }
